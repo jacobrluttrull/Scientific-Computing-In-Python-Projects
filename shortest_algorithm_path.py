7a62436b89b2758b989222a3e91409da40df674a
Jacob Luttrull <jacobrluttrull@gmail.com>
2025-07-18 01:42:03 -0400

    on step 45 of shortest_algorithm_path.py going to complete later on

--- shortest_algorithm_path.py
+++ shortest_algorithm_path.py
@@ -1,7 +1,21 @@
 my_graph = {
-    'A': ['B', 'D'],
-    'B': ['A', 'C'],
-    'C': ['B', 'D'],
-    'D': ['A', 'C']
+    'A': [('B', 3), ('D', 1)],
+    'B': [('A', 3), ('C', 4)],
+    'C': [('B', 4), ('D', 7)],
+    'D': [('A', 1), ('C', 7)]
 }
-#On step 20 not a lot of code so far
+
+
+def shortest_path(graph, start):
+    unvisited = list(graph)
+    distances = {node: 0 if node == start else float('inf') for node in graph}
+    paths = {node: [] for node in graph}
+    paths[start].append(start)
+    while unvisited:
+        current = min(unvisited, key=distances.get)
+        for node, distance in graph[current]:
+            if distance + distances[current] < distances[node]:
+                distances[node] = distance + distances[current]
+    print(f'Unvisited: {unvisited}\nDistances: {distances}\nPaths: {paths}')
+
+# shortest_path(my_graph, 'A')
