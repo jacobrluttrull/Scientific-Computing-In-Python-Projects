07a6d08df5ca08107145322c115bbc06195e5240
Jacob Luttrull <jacobrluttrull@gmail.com>
2025-08-09 17:51:41 -0400

    refactor vector class to R2Vector and R3Vector; enhance norm calculation and string representation

--- vector_space.py
+++ vector_space.py
@@ -1,15 +1,27 @@
-class Vector:
-    def __init__(self, x, y):
+class R2Vector:
+    def __init__(self, *, x, y):
         self.x = x
         self.y = y
 
     def norm(self):
-        return (self.x ** 2 + self.y ** 2) ** 0.5
+        return sum(val ** 2 for val in vars(self).values()) ** 0.5
 
     def __str__(self):
-        return f'{self.x, self.y}'
+        return str(tuple(getattr(self, i) for i in vars(self)))
+
+    def __repr__(self):
+        arg_list = [f'{key}={val}' for key, val in vars(self).items()]
+        args = ', '.join(arg_list)
+        return f'{self.__class__.__name__}({args})'
 
 
-v1 = Vector(2, 3)
-print(v1.norm())
-print(v1)
+class R3Vector(R2Vector):
+    def __init__(self, *, x, y, z):
+        super().__init__(x=x, y=y)
+        self.z = z
+
+
+v1 = R2Vector(x=2, y=3)
+v2 = R3Vector(x=2, y=2, z=3)
+# print(f'v1 = {v1}', f'\nrepr = {repr(v1)}')
+# print(f'v2 = {v2}', f'\nrepr = {repr(v2)}')
