d66a89887e4c47fd518a7647468bfdba53381c5d
Jacob Luttrull <jacobrluttrull@gmail.com>
2025-09-09 20:32:21 -0400

    add projectile motion calculation class

---
+++ projectile_calc.py
@@ -0,0 +1,24 @@
+import math
+
+GRAVITATIONAL_ACCELERATION = 9.81
+PROJECTILE = "∙"
+x_axis_tick = "T"
+y_axis_tick = "⊣"
+
+
+class Projectile:
+    __slots__ = ('__speed', '__height', '__angle')
+
+    def __init__(self, speed, height, angle):
+        self.__speed = speed
+        self.__height = height
+        self.__angle = math.radians(angle)
+
+    def __calculate_displacement(self):
+        horizontal_component = self.__speed * math.cos(self.__angle)
+        vertical_component = self.__speed * math.sin(self.__angle)
+        squared_component = vertical_component ** 2
+        gh_component = 2 * GRAVITATIONAL_ACCELERATION * self.__height
+        sqrt_component = math.sqrt(squared_component + gh_component)
+
+        return horizontal_component * (vertical_component + sqrt_component) / GRAVITATIONAL_ACCELERATION
