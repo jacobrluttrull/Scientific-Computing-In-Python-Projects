0c9d2689ddd28846847a4d9bbebfa21da11cf4fb
Jacob Luttrull <jacobrluttrull@gmail.com>
2025-09-11 18:53:25 -0400

    refactor projectile class to use properties and add Graph class for trajectory representation

--- projectile_calc.py
+++ projectile_calc.py
@@ -17,9 +17,9 @@ class Projectile:
     def __str__(self):
         return f'''
 Projectile details:
-speed: {self.__speed} m/s
-height: {self.__height} m
-angle: {round(math.degrees(self.__angle))}°
+speed: {self.speed} m/s
+height: {self.height} m
+angle: {self.angle}°
 displacement: {round(self.__calculate_displacement(), 1)} m
 '''
 
@@ -47,7 +47,61 @@ displacement: {round(self.__calculate_displacement(), 1)} m
             for x in range(math.ceil(self.__calculate_displacement()))
         ]
 
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def angle(self):
+        return round(math.degrees(self.__angle))
+
+    @property
+    def speed(self):
+        return self.__speed
+
+    @height.setter
+    def height(self, n):
+        self.__height = n
+
+    @angle.setter
+    def angle(self, n):
+        self.__angle = math.radians(n)
+
+    @speed.setter
+    def speed(self, s):
+        self.__speed = s
+
+    def __repr__(self):
+        return f'{self.__class__}({self.speed}, {self.height}, {self.angle})'
+
+
+class Graph:
+    __slots__ = ('__coordinates')
+
+    def __init__(self, coord):
+        self.__coordinates = coord
+
+    def __repr__(self):
+        return f"Graph({self.__coordinates})"
+
+    def create_coordinates_table(self):
+        table = '\n  x      y\n'
+        for x, y in self.__coordinates:
+            table += f'{x:>3}{y:>7.2f}\n'
+
+        return table
+
+    def create_trajectory(self):
+        rounded_coords = [(round(x), round(y)) for x, y in self.__coordinates]
+
+        x_max = max(rounded_coords, key=lambda i: i[0])[0]
+        y_max = max(rounded_coords, key=lambda j: j[1])[1]
+
+        return x_max, y_max
+
 
 ball = Projectile(10, 3, 45)
 print(ball)
 coordinates = ball.calculate_all_coordinates()
+graph = Graph(coordinates)
+print(graph.create_trajectory())
